6885aa2f86a193448c8a6abb399b901887801164
Gabriel de Santana <gabrieldesantana.contato@gmail.com>
2022-05-01 00:08:49 -0300

    commit inicial

---
+++ Pages/Conversa.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SignalRChat.Pages
+{
+    public class ConversaModel : PageModel
+    {
+        private readonly ILogger<PrivacyModel> _logger;
+
+        public ConversaModel(ILogger<PrivacyModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
